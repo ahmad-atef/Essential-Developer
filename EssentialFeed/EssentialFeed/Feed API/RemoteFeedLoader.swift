9c4d6c9cf258ff91cba738f3bbb1778717136a9e
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-05-23 15:51:33 +0200

    Guarantee we only receive one error + more code clean up.

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -5,8 +5,16 @@
 //  Created by Ahmed Atef Ali Ahmed on 14.05.21.
 //
 
+public protocol HTTPClient {
+    func request(from url: URL, completion: (Error) -> Void)
+}
 
 public final class RemoteFeedLoader {
+
+    /// Remote Feed Loader domain Errors.
+    public enum Error: Swift.Error {
+        case connectivity
+    }
 
     private let url: URL
     private let client: HTTPClient
@@ -16,11 +24,11 @@ public final class RemoteFeedLoader {
         self.client = client
     }
 
-    public func load() {
-        client.request(from: url)
+    public func load(completion: (RemoteFeedLoader.Error) -> Void = { _ in }) {
+        client.request(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
 
-public protocol HTTPClient {
-    func request(from url: URL)
-}
+
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,25 +8,17 @@
 import XCTest
 import EssentialFeed
 
-final class HTTPSpyClient: HTTPClient {
-    var requestedURLs: [URL?] = []
-
-    func request(from url: URL){
-        requestedURLs.append(url)
-    }
-}
-
-class RemoteFeedLoaderTests: XCTestCase {
+final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromClient() {
-        let url = URL(string: "https://a-given-url.com")!
+        let url: URL = .given
         let client = makeSpyClientAndRemoteLoader(from: url).client
 
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     func test_load_requestsDataURLFromClient() {
-        let url = URL(string: "https://a-given-url.com")!
+        let url: URL = .given
         let (client, loader) = makeSpyClientAndRemoteLoader(from:url)
 
         loader.load()
@@ -35,7 +27,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     func testOneLoadDoesNotLoadMoreThanOneTime() {
-        let url = URL(string: "https://a-given-url.com")!
+        let url: URL = .given
         let (client, loader) = makeSpyClientAndRemoteLoader(from:url)
 
         loader.load()
@@ -44,7 +36,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     func test_loadTwice_requestDataFromClientTwice() {
-        let url = URL(string: "https://a-given-url.com")!
+        let url: URL = .given
         let (client, loader) = makeSpyClientAndRemoteLoader(from:url)
 
         loader.load()
@@ -54,9 +46,45 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
+    func testLoaderReturnsExpectedErrorWhenClientFails() {
+        // Arrange
+        let url: URL = .given
+        let (client, loader) = makeSpyClientAndRemoteLoader(from:url)
+        client.error = NSError.testing
+
+        // Act
+        var capturedErrors: [RemoteFeedLoader.Error] = []
+        loader.load { capturedErrors.append($0) }
+
+        // Assert
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+
     private func makeSpyClientAndRemoteLoader(from url: URL) -> (client: HTTPSpyClient, loader: RemoteFeedLoader) {
         let spyClient = HTTPSpyClient()
         let loader = RemoteFeedLoader(url: url, client: spyClient)
         return(spyClient, loader)
     }
+}
+
+
+final class HTTPSpyClient: HTTPClient {
+
+    var requestedURLs: [URL?] = []
+    var error: Error?
+
+    func request(from url: URL, completion: (Error) -> Void) {
+        if let error = error {
+            completion(error)
+        }
+        requestedURLs.append(url)
+    }
+}
+
+private extension URL {
+    static let given = URL(string: "https://a-given-url.com")!
+}
+
+private extension NSError {
+    static let testing = NSError(domain: "test", code: 0)
 }
