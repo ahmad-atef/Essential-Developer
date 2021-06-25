d6fd4873a491f6d8dd969088f925bf6d941334ab
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-06-25 09:37:32 +0200

    Update end-to-end timeout to 10 seconds.
    Signed-off-by: Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -27,9 +27,9 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
                 items.enumerated().forEach { index, item in
                     XCTAssertEqual(item.id, self.expectedFeedItem(at: index).id)
-//                    XCTAssertEqual(item.description, self.expectedFeedItem(at: index).description)
-//                    XCTAssertEqual(item.imageURL, self.expectedFeedItem(at: index).imageURL)
-//                    XCTAssertEqual(item.location, self.expectedFeedItem(at: index).location)
+                    XCTAssertEqual(item.description, self.expectedFeedItem(at: index).description)
+                    XCTAssertEqual(item.imageURL, self.expectedFeedItem(at: index).imageURL)
+                    XCTAssertEqual(item.location, self.expectedFeedItem(at: index).location)
                 }
 
             case .failure(let error):
@@ -37,7 +37,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             }
             expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 5.0)
+        wait(for: [expectation], timeout: 10.0)
     }
 
     private func expectedFeedItem(at index: Int, file: StaticString = #filePath, line: UInt = #line) -> FeedItem {
@@ -62,33 +62,33 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             ),
             FeedItem(
                 id: .init(uuidString: "FF0ECFE2-2879-403F-8DBE-A83B4010B340")!,
-                description: "Description 1",
-                location: "Location 1",
-                imageURL: URL(string: "https://url-1.com")!
+                description: nil,
+                location: nil,
+                imageURL: URL(string: "https://url-4.com")!
             ),
             FeedItem(
                 id: .init(uuidString: "DC97EF5E-2CC9-4905-A8AD-3C351C311001")!,
-                description: "Description 1",
-                location: "Location 1",
-                imageURL: URL(string: "https://url-1.com")!
+                description: "Description 5",
+                location: "Location 5",
+                imageURL: URL(string: "https://url-5.com")!
             ),
             FeedItem(
                 id: .init(uuidString: "557D87F1-25D3-4D77-82E9-364B2ED9CB30")!,
-                description: "Description 1",
-                location: "Location 1",
-                imageURL: URL(string: "https://url-1.com")!
+                description: "Description 6",
+                location: "Location 6",
+                imageURL: URL(string: "https://url-6.com")!
             ),
             FeedItem(
                 id: .init(uuidString: "A83284EF-C2DF-415D-AB73-2A9B8B04950B")!,
-                description: "Description 1",
-                location: "Location 1",
-                imageURL: URL(string: "https://url-1.com")!
+                description: "Description 7",
+                location: "Location 7",
+                imageURL: URL(string: "https://url-7.com")!
             ),
             FeedItem(
                 id: .init(uuidString: "F79BD7F8-063F-46E2-8147-A67635C3BB01")!,
-                description: "Description 1",
-                location: "Location 1",
-                imageURL: URL(string: "https://url-1.com")!
+                description: "Description 8",
+                location: "Location 8",
+                imageURL: URL(string: "https://url-8.com")!
             )
         ][index]
     }
