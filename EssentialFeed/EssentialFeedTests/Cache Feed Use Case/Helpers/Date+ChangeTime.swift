d703b1a51e5649ba4c1a7058bd2096cb0e3a54d5
Ahmad Atef <ahmda.atef.ali.ahmad@gmail.com>
2022-03-04 18:55:34 +0100

    Separate Date extension based on context.

--- EssentialFeed/EssentialFeedTests/Cache Feed Use Case/Helpers/Date+ChangeTime.swift
+++ EssentialFeed/EssentialFeedTests/Cache Feed Use Case/Helpers/Date+ChangeTime.swift
@@ -1,15 +1,7 @@
-//
-//  Date+ChangeTime.swift
-//  EssentialFeedTests
-//
-//  Created by Ahmed Atef Ali Ahmed on 10.09.21.
-//
-
 import Foundation
 
 extension Date {
-
-    private func changeTime(byAddingDays days: Int) -> Date {
+    private func adding(days: Int) -> Date {
         var dateComponent = DateComponents()
         dateComponent.day = days
         return Calendar(identifier: .gregorian).date(byAdding: dateComponent, to: self)!
@@ -21,9 +13,11 @@ extension Date {
 
     /// DSL for MaxAge for feed cache.
     public func minusFeedCacheMaxAge() -> Date {
-        changeTime(byAddingDays: feedCacheMaxAgeInDays)
+        adding(days: feedCacheMaxAgeInDays)
     }
+}
 
+extension Date {
     public func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
