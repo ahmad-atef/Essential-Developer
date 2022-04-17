16d3f93000439c9cdd1df8a1159c085abbacd2a6
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2022-04-18 01:16:28 +0200

    Update timeout for deletion because it takes longer.

--- EssentialFeed/EssentialFeedTests/Cache Feed/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Cache Feed/CodableFeedStoreTests.swift
@@ -241,7 +241,7 @@ final class CodableFeedStoreTests: XCTestCase {
             expectedError = error
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 3.0)
         return expectedError
     }
 }
