a5b372cd838ffe3fb8f227039d7b520fb4b37f4d
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2022-05-10 14:39:35 +0200

    XCTesCase+FeedStoreSpecs

--- EssentialFeed/EssentialFeedTests/Cache Feed/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Cache Feed/CodableFeedStoreTests.swift
@@ -262,59 +262,6 @@ final class CodableFeedStoreTests: XCTestCase, FeedStoreSpecs, FailableFeedStore
             .first!
             .appendingPathComponent("\(type(of: self)).store")
     }
-
-    private func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: RetrieveFeedResult, file: StaticString = #file, line: UInt = #line) {
-        expect(sut, toRetrieve: expectedResult)
-        expect(sut, toRetrieve: expectedResult)
-    }
-
-    private func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrieveFeedResult, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Waiting for cache to retrieve")
-
-        sut.retrieve { retrievedResult in
-            switch (retrievedResult, expectedResult) {
-            case (.empty, .empty): break // ✅
-            case (.failure, .failure): break // ✅
-            case let (.found(retrievedItems, retrievedDate), .found(expectedItems, expectedDate)):
-                XCTAssertEqual(retrievedItems, expectedItems)
-                XCTAssertEqual(retrievedDate, expectedDate)
-            default:
-                XCTFail(
-                    "Expected to retrieve \(expectedResult), got \(retrievedResult) instead",
-                    file: file,
-                    line: line
-                )
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-    }
-
-    @discardableResult
-    private func insert(_ cache: [LocalFeedImage], timeStamp: Date, to sut: FeedStore) -> Error? {
-        let exp = expectation(description: "Waiting for cache insertion")
-        var expectedError: Error?
-
-        sut.insert(cache, timeStamp: timeStamp) { error in
-            expectedError = error
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-        return expectedError
-    }
-
-    @discardableResult
-    private func deleteCache(from sut: FeedStore) -> Error? {
-        let exp = expectation(description: "waiting for deletion to finish")
-        var expectedError: Error?
-
-        sut.deleteCachedFeed { error in
-            expectedError = error
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 3.0)
-        return expectedError
-    }
 }
 
 private extension URL {
